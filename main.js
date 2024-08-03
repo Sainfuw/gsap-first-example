7d10c4992736c74f0f87a1ebeb656492bc99490c
Emilio Navarro <sainfuw@outlook.com>
2024-08-02 22:51:16 -0400

    gsap with parcel working

---
+++ main.js
@@ -0,0 +1,28 @@
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Lenis from 'lenis'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+})
+
+function raf (time) {
+  lenis.raf(time)
+  window.requestAnimationFrame(raf)
+}
+
+window.requestAnimationFrame(raf)
+
+gsap.timeline({
+  scrollTrigger: {
+    trigger: '.img',
+    scrub: true
+  }
+})
+  .to('.img', {
+    stagger: 0.2,
+    y: -700
+  })
